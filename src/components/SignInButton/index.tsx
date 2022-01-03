079953d69dfb2aed305fad937e199f0cd1a68abd
Sabrina <sah.luciana@gmail.com>
2022-01-03 00:35:51 -0300

    :construction: added style, header component created and working on button state

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,21 @@
+import styles from "./styles.module.scss";
+import { FaGithub } from "react-icons/fa";
+import { useState } from "react";
+import { FiX } from "react-icons/fi";
+
+export function SignInButton() {
+  const [isLogged, setLogged] = useState(true);
+
+  return isLogged ? (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#84d361" />
+      Sign In with Github
+      <FiX color="#737380" className={styles.closeIcon}/>
+    </button>
+  ) : (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#eba417" />
+      Sign In with Github
+    </button>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,23 @@
-
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
-     <>
-     <Head>
+    <>
+      <Head>
         <title> Início| ig.news</title>
-    </Head>
-    <h1>
-     Hello Wold
-    </h1>
-     </>
-  )
+      </Head>
+      <h1>Hello Wold</h1>
+    <main>
+      <section>
+        <span>👏 Hey, welcome!</span>
+        <h1>News about the <span>React</span> world.</h1>
+        <p>
+          Get access to all the publications <br/>
+          <span>for R$ 10,00 month</span>
+        </p>
+      </section>
+      <img src="/images/avatar.svg" alt="Girl coding" />
+    </main>
+    </>
+  );
 }

cbb2aaf56182aeb1134090cb0538e7ce506fa66e
SabrinaVieira <sah.luciana@gmail.com>
2022-01-17 23:46:34 -0300

    :consstruction: checkpoint: autenticaçao com github, corrigir erro next [next-auth][error][OAUTH_CALLBACK_ERROR] https://next-auth.js.org/errors#oauth_callback_error outgoing request timed out after 3500ms

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -2,18 +2,21 @@ import styles from "./styles.module.scss";
 import { FaGithub } from "react-icons/fa";
 import { useState } from "react";
 import { FiX } from "react-icons/fi";
+import { useSession, signIn, signOut  } from 'next-auth/react';
 
 export function SignInButton() {
-  const [isLogged, setLogged] = useState(true);
+//   const [isLogged, setLogged] = useState(true);
+const { data: session } = useSession();
+console.log({session})
 
-  return isLogged ? (
-    <button type="button" className={styles.signInButton}>
+  return session ? (
+    <button type="button" className={styles.signInButton} onClick={()=> signOut()}>
       <FaGithub color="#84d361" />
       Sabrina Vieira
       <FiX color="#737380" className={styles.closeIcon}/>
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button type="button" className={styles.signInButton} onClick={()=> signIn('github')}>
       <FaGithub color="#eba417" />
       Sign In with Github
     </button>
--- src/pages/api/example.ts
+++ src/pages/api/example.ts
@@ -1,6 +1,13 @@
 /* eslint-disable import/no-anonymous-default-export */
 import { NextApiRequest, NextApiResponse } from "next"
 
+// formas de autenticação no next
+// JWT  (Storage)
+// Next Auth (Social)
+// providers de autenticação externa: Cognito, Auth0
+// Autenticação de email e senha
+
+
 export default (request: NextApiRequest, response: NextApiResponse) => {
     const users = [
         {id: 1, name: 'Diego'},
@@ -9,3 +16,5 @@ export default (request: NextApiRequest, response: NextApiResponse) => {
 
     return response.json(users)
 }
+
+// http://localhost:3000/api/users/
